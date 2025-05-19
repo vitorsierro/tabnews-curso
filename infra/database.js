a8938ae66ed57747baf7c12dd06e428ce105c55a
vitor <vitorsierro_@hotmail.com>
2025-05-19 20:17:44 -0300

    feat: database try catch

--- infra/database.js
+++ infra/database.js
@@ -1,19 +1,23 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  });
-  await client.connect();
-  const result = await client.query(queryObject);
+  try {
+    const client = new Client({
+      host: process.env.POSTGRES_HOST,
+      port: process.env.POSTGRES_PORT,
+      user: process.env.POSTGRES_USER,
+      database: process.env.POSTGRES_DB,
+      password: process.env.POSTGRES_PASSWORD,
+    });
+    await client.connect();
+    const result = await client.query(queryObject);
 
-  await client.end();
-  console.log(result);
-  return result;
+    return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
