b351e7da032ebf530165348959a5f7943c4be690
vitor <vitorsierro_@hotmail.com>
2025-05-21 20:23:01 -0300

    feat: update database prod

--- infra/database.js
+++ infra/database.js
@@ -1,20 +1,26 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl:
+      process.env.NODE_ENV === "development"
+        ? false
+        : { rejectUnauthorized: false },
+  });
+
   try {
-    const client = new Client({
-      host: process.env.POSTGRES_HOST,
-      port: process.env.POSTGRES_PORT,
-      user: process.env.POSTGRES_USER,
-      database: process.env.POSTGRES_DB,
-      password: process.env.POSTGRES_PASSWORD,
-    });
     await client.connect();
     const result = await client.query(queryObject);
 
     return result;
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
